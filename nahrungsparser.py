5df49546508ffd9067a63b23aa68c726cd14a49d
Leon Bartle <leon@bartle.eu>
2021-09-14 15:06:17 +0200

    Update nahrungsparser.py

--- nahrungsparser.py
+++ nahrungsparser.py
@@ -1,6 +1,18 @@
 import time
 import urllib.request
 import os
+from datetime import datetime
 
-url ="https://cdn3.qnips.com/release-menu-pdfs/Mittagessen_DE_37_20210913_668875.pdf"
+url ="https://cdn3.qnips.com/release-menu-pdfs/Mittagessen_DE_"+"37_20210913"+"_"+"668875"+".pdf"
 urllib.request.urlretrieve(url, "tmp/tmp.pdf")
+
+
+
+print(os.system('python pdf2txt.py tmp/tmp.pdf'))
+
+time.sleep(5)
+
+if os.path.exists("tmp/tmp.pdf"):
+  os.remove("tmp/tmp.pdf")
+else:
+  print("Du hast hart verkaggert")
