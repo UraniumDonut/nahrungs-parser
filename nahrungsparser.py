a7bc7df7bbd35dcf93a68b57b1682901d094baa8
Leon Bartle <leon@bartle.eu>
2021-09-14 14:10:09 +0200

    html test

--- nahrungsparser.py
+++ nahrungsparser.py
@@ -1,6 +1,11 @@
-
+from requests_html import HTMLSession
+import time, requests
 print("nahrungsparser gestartet")
 
 
 urlsp6 = "aubauen hier"+"extra"
 
+nodes = requests.get("https://www.todayonline.com/api/v3/news_feed/7")\
+        .json()["nodes"]
+for node in nodes:
+    print(node["node"]["title"])
